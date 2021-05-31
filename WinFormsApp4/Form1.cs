ee4cf8f11c81b155a813d8d6bc1a0917f514ae53
Danylo Klymovskyi <daniil.klimovskiy@gmai.com>
2021-05-31 23:22:38 +0300

    new

---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float x = float.Parse(textBox1.Text);
+                float y = float.Parse(textBox2.Text);
+                float z = float.Parse(textBox3.Text);
+                textBox4.Text = (x + y + z).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wypełnij wszystkie pola liczbami");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float x = float.Parse(textBox1.Text);
+                float y = float.Parse(textBox2.Text);
+                float z = float.Parse(textBox3.Text);
+                textBox4.Text = (x * y * z).ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wypełnij wszystkie pola liczbami");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
